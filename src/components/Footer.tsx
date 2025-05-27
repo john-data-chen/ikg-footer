31a5eec8e4770c671abe01d44c32710df6bd3b06
JianAn Chen <john.data.chen@gmail.com>
2025-05-27 17:20:04 +0800

    style(Footer): adjust text styling for better readability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer: React.FC = () => {
         <h2 className="mb-4 text-center font-[Outfit] text-[48px] font-bold tracking-tight text-[#000000] uppercase">
           Don’t Miss Out
         </h2>
-        <p className="mb-8 max-w-2xl text-center font-[Outfit] text-[24px] font-normal tracking-tight text-[#494706]">
+        <p className="mb-8 max-w-2xl text-center font-[Outfit] text-[24px] leading-none font-normal tracking-[-0.02em] text-[#494706]">
           Get the latest news and updates delivered straight to your inbox.
         </p>
         <form className="flex h-[100px] w-[688px] items-center gap-2.5 rounded-[70px] border border-black bg-white px-6 py-2 shadow-md">
